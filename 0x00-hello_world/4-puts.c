82fe9a0fb492bd7e5ea170b527b1970174538084
Hlamulo1 <chlamulo1@gmail.com>
2022-09-12 21:50:39 -0700

    puts funtion

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+*main -print a string using the put function
+*description - main fuction usage
+*the program print out "Programming is like building a multilingual puzzle"
+*return :0
+*/
+
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return 0;
+}
