4c0402cf856a9cc8d00b97645b17b35c1529830d
Hlamulo1 <chlamulo1@gmail.com>
2022-09-14 18:51:17 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,34 @@
-#include "main.h"                                                                                                                                                         
+#include "main.h"
 
-                                                                                                                                                                          
+/**
+*main - prints putchar
+*return: always 0
+*/
 
-/**                                                                                                                                                                       
+int main(void)
 
-*main _ print putchar                                                                                                                                                     
+{
 
-*return: zero                                                                                                                                                             
+_putchar('_');
 
-*/                                                                                                                                                                        
-
-int main(void)                                                                                                                                                            
-
-_putchar('-');
 _putchar('p');
+
 _putchar('u');
+
 _putchar('t');
+
 _putchar('c');
+
 _putchar('h');
+
 _putchar('a');
+
 _putchar('r');
+
 _putchar('\n');
 
-return 0;
+
+
+return (0)
+
 }
