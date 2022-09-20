73b03ff440cd1744ccc070244cc06044820eac1e
Hlamulo1 <chlamulo1@gmail.com>
2022-09-19 19:44:06 -0700

    correction

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,15 @@
-#include "main.h"
+#include "main.h" 
 
-/**
-*swap_int - swaps two 'interger '
-*@a: first interger
-*@b: second interger
-*return: returns nothing
-*/
+/** 
+*swap_int - swaps two interger's values
+*@a: first interger   
+*@b: second interger 
+*return: returns nothing 
+*/   
 
-void swap_int( int *a, int *b)
-(
-      int tmp = *a;
-    
-   *a = *b;
-    *b = tmp;
-}
+void swap_int( int *a, int *b)    
+{
+int tmp = *a;  
+
+    *a = *b; 
+    *b = tmp;   
