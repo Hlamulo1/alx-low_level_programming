943ac202656f5186494a002b431099d36f708f20
Hlamulo1 <chlamulo1@gmail.com>
2022-09-19 19:38:16 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*swap_int - swaps two 'interger '
+*@a: first interger
+*@b: second interger
+*return: returns nothing
+*/
+
+void swap_int( int *a, int *b)
+(
+      int tmp = *a;
+    
+   *a = *b;
+    *b = tmp;
+}
